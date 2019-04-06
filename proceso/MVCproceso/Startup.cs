5ac43974df26a529720c79cb2bb8d2e3d598a744
Jacqueline montano <Jacqueline montano@DESKTOP-9LLNFII>
2019-04-05 23:09:39 -0400

    commit proyecto mvc

---
+++ proceso/MVCproceso/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCproceso.Startup))]
+namespace MVCproceso
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
